3a811c29be59f9ac5ca6909a2fa76bae7a108f30
Matheus Gomes <matheus_mgc@yahoo.com.br>
2025-07-24 02:20:40 -0300

    adiciona animacao e cores

--- src/app/components/loginScreen/loginScreen.tsx
+++ src/app/components/loginScreen/loginScreen.tsx
@@ -7,9 +7,9 @@ import './loginScreen.scss';
 
 export default function LoginScreen() {
     const [isActive, setIsActive] = useState(false);
-    
+
     return (
-        <div className="container" id="container">
+        <div className={`container ${isActive ? 'active' : ''}`} id="container">
             <div className="form-container sign-up">
                 <form className="formUp">
                     <h1>Criar uma conta</h1>
@@ -20,9 +20,9 @@ export default function LoginScreen() {
                         <a href="#" className="icon"><FaLinkedinIn /></a>
                     </div>
                     <span>ou use seu e-mail para registro</span>
-                    <input className="input" type="text" placeholder="Name"/>
-                    <input className="input" type="email" placeholder="Email"/>
-                    <input className="input" type="password" placeholder="Password"/>
+                    <input className="input" type="text" placeholder="Name" />
+                    <input className="input" type="email" placeholder="Email" />
+                    <input className="input" type="password" placeholder="Password" />
                     <button>Cadastre-se</button>
                 </form>
             </div>
@@ -35,9 +35,9 @@ export default function LoginScreen() {
                         <a href="#" className="icon"><FaGithub /></a>
                         <a href="#" className="icon"><FaLinkedinIn /></a>
                     </div>
-                    <span>ou use sua senha de e-mail</span>
-                    <input className="input" type="email" placeholder="Email"/>
-                    <input className="input" type="password" placeholder="Password"/>
+                    <span>ou use seu e-mail e senha</span>
+                    <input className="input" type="email" placeholder="Email" />
+                    <input className="input" type="password" placeholder="Password" />
                     <a href="#">Esqueceu sua senha?</a>
                     <button>Entrar</button>
                 </form>
@@ -47,12 +47,12 @@ export default function LoginScreen() {
                     <div className="toggle-panel toggle-left">
                         <h1>Bem vindo de volta!</h1>
                         <p>Insira seus dados pessoais para usar todos os recursos do site</p>
-                        <button className="hidden" id="login">Entrar</button>
+                        <button className="hidden" onClick={() => setIsActive(false)}>Entrar</button>
                     </div>
                     <div className="toggle-panel toggle-right">
                         <h1>Olá amigo!</h1>
                         <p>Cadastre-se com seus dados pessoais para usar todos os recursos do site</p>
-                        <button className="hidden" id="register">Cadastre-se</button>
+                        <button className="hidden" onClick={() => setIsActive(true)}>Cadastre-se</button>
                     </div>
                 </div>
             </div>
